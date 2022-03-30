0484bbbfce5cdaa5440deabd19494f81e9dcc6a0
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-03-30 13:52:17 -0400

    added the JavaScript code

--- src/main/java/com/example/flowerdataservice/data/jpa/FlowerDataRepository.java
+++ src/main/java/com/example/flowerdataservice/data/jpa/FlowerDataRepository.java
@@ -3,6 +3,6 @@ package com.example.flowerdataservice.data.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:63342/", "http://localhost:4200/"})
 public interface FlowerDataRepository extends JpaRepository<Flower,String> {
 }
